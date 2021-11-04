228bb820d8db648b7918fe4022b473b99f746e7c
Pourya Akrami <pourya.ak1996@gmail.com>
2021-11-05 02:06:23 +0800

    added basic routing to the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import { Switch, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const App = () => {
   return (
@@ -9,7 +11,10 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen />
+          <Switch>
+            <Route path="/" exact component={HomeScreen} />
+            <Route path="/product/:id" component={ProductScreen} />
+          </Switch>
         </Container>
       </main>
       <Footer />
