b9535e300b6fe0c70f4d8089423447101b145939
Pourya Akrami <pourya.ak1996@gmail.com>
2021-11-05 14:45:57 +0800

    configured env file and installed dotenv package

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const products = require("./data/products");
 
+dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("API is running!");
@@ -16,4 +20,7 @@ app.get("/api/products/:id", (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log("Server listening on port 5000"));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
