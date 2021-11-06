7700ba0544fb77f91f53863a7bd6a1a633a8aa2b
Pourya Akrami <pourya.ak1996@gmail.com>
2021-11-06 12:51:16 +0800

    Installed and configured redux store

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import { BrowserRouter } from "react-router-dom";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
