349969254611b971c88549859a6d4069f6e854ab
Pourya Akrami <pourya.ak1996@gmail.com>
2021-11-10 20:34:12 +0800

    Created Orders controllers and routes

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 // Error Handlers Middleware
 app.use(notFound);
